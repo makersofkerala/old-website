57d7dc22df1e3143a57cbd7115a4e8b4d925c8a0
Prathyush <prathyushpramod@gmail.com>
2018-10-31 06:13:18 +0530

    Fix meta tags and time tag

--- view.mjs
+++ view.mjs
@@ -8,7 +8,7 @@ const event = ({title = "Untitled Event", location: {lat, long, venue}, date = "
     const {day, year} = {day: parsedDate.getDate(), year: parsedDate.getFullYear()};
     
     return ["a.event-block", {href: url},
-	    ["div.event-date", ["time.event-day", day], ["time.event-month", month], year],
+	    ["div.event-date", ["p.event-day", day], ["p.event-month", month], ["p.event-year", year]],
 	    ["div.event-details",  ["div.event-name", title],
 	     ["div.event-location", ["img.location-icon", {alt: "location pin", src: "location.svg"}], venue]],
 	    ["div", "→ "]];
@@ -22,8 +22,8 @@ const page = (events) => ["div.website",
 ["ul", ["li", ...events.map(eObj => event(eObj))]]];
 
 
-const website = (events) => z.doc([["meta", {title: "Makers of Kerala"}],
-["meta", {description: "A website to unite the daring makers in the digital domain all across the Kerala, India. Makers of Kerala is designed as a platform that catalogues the team, stack, and chronicles the timeline of startups from Kerala. It's overarching goal is to chronicle the unfolding narrative of the startup scene of Kerala."}],
+const website = (events) => z.doc([["meta", {name: "title", "content": "Makers of Kerala"}],
+["meta", {name: "description", content: "A website to unite the daring makers in the digital domain all across the Kerala, India. Makers of Kerala is designed as a platform that catalogues the team, stack, and chronicles the timeline of startups from Kerala. It's overarching goal is to chronicle the unfolding narrative of the startup scene of Kerala."}],
 ["meta", {charset: "utf-8"}],
 ["link", {href: "https://fonts.googleapis.com/css?family=Work+Sans:400,600", rel:"stylesheet"}],
 z.css("ui.css")], [page(events)]);
