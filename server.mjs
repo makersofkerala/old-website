4e29d8db583204f6cb87d45a3e1b0d91c3664d6f
Prathyush <prathyushpramod@gmail.com>
2018-10-31 05:36:15 +0530

    Serve from public folder

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import * as xila from 'xila';
 
 const PORT = process.argv.slice(2)[0] || 8000;
 
-let server = xila.serve(xila.get("/*", xila.staticAssets("./")));
+let server = xila.serve(xila.get("/*", xila.staticAssets("./public/")));
 
 console.log("Starting server on " + PORT);
 
